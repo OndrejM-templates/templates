011eaff0b6e7b7b4a1f0400c7c40a52595d2446f
OpenShift guest <ondrej.mihalyi@gmail.com>
2013-09-23 04:21:51 -0400

    Dynamically generate getter/setter method for object

---
+++ javascript/dojo/dojoconfig.js
@@ -0,0 +1,20 @@
+
+window.dojoConfig = {
+    async: 1
+};
+
+/* To include local package when not relative to dojo root, example: dojoConfig = {packages: [localPackage('js', 'package/subpackage')]}
+@param srcRoot path of source root relative to html file url
+@param name of the package and also path to the package within srcRoot path
+@return element into array of dojoConfig packages - {name, location}
+*/
+function localPackage(srcRoot, name){
+    var retLoc = location.pathname.replace(/\/[^/]*$/, '') + '/' ;
+    if (srcRoot) {
+        retLoc += srcRoot + '/';
+    }
+    return {
+        name: name,
+        location: retLoc + name
+    };
+}
